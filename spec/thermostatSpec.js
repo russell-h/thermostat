943c3e8688423e5d89f3e10fbf9c08f0fff2ee0e
Jenny <jennybell42@gmail.com>
2021-06-03 14:38:33 +0100

    first commit

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Thermostat', () => {
+  
+  let thermostat;
+
+beforeEach(() => {
+  thermostat = new Thermostat();
+});  
+
+it('starts at 20 degrees', () => {
+  expect(thermostat.getCurrentTemperature()).toEqual(20);
+});
+
+});
---
+++ src/thermostat.js
@@ -0,0 +1,8 @@
+class Thermostat {
+ constructor() {
+   this.temperature = 20
+ }
+  getCurrentTemperature() {
+    return this.temperature
+  }
+}
